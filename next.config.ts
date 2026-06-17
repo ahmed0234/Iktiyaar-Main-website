d6e503a3c5ab6a84fab463268442ce0388eb2821
ahmed <ahmedkiller0234@gmail.com>
2026-06-17 05:25:19 +0500

    image remote pattern

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,15 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
-export default nextConfig;  
+export default nextConfig;
